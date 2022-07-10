34faae809ddbf24c2faff1bc4a4bedad75599cbb
p-vascool <vasko_pan1@mail.bg>
2022-07-10 16:27:07 +0300

    Add

---
+++ Services/Company.Services.Data/Contracts/ITripreqeustService.cs
@@ -0,0 +1,14 @@
+﻿namespace Company.Services.Data.Contracts
+{
+    using System.Collections.Generic;
+
+    using Company.Data.Models;
+
+    public interface ITripreqeustService
+    {
+        TripRequest GetById(string id);
+
+        IEnumerable<TripRequest> GetAllTripRequests();
+
+    }
+}
--- Web/Company.Web.ViewModels/Destinations/ViewModels/DestinationViewModel.cs
+++ Web/Company.Web.ViewModels/Destinations/ViewModels/DestinationViewModel.cs
@@ -2,5 +2,10 @@
 {
     public class DestinationViewModel
     {
+        public int Id { get; set; }
+
+        public string Region { get; set; }
+
+        public string Name { get; set; }
     }
 }
